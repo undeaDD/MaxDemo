5852098a0a52e00ac6f35af09212c7468c05cfeb
Dominic Drees <dominic.drees@live.de>
2022-02-21 23:05:25 +0100

    styling

--- App.js
+++ App.js
@@ -1,17 +1,55 @@
 import React from "react";
+import { BlurView } from "expo-blur";
+import Constants from "expo-constants";
+import { StatusBar } from 'expo-status-bar';
 import { Home, HomeOptions } from "./screens/home/Home";
+import { useColorScheme, StyleSheet } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, DarkTheme } from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const scheme = useColorScheme();
+	const theme = scheme === "dark" ? DarkTheme : DefaultTheme;
+
 	return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} options={HomeOptions} />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: theme.colors.text,
+          tabBarInactiveTintColor: "#808080",
+          tabBarStyle: { position: "absolute" },
+          tabBarBackground: () => (
+            <BlurView 
+              tint={scheme}
+              intensity={100}
+              style={StyleSheet.absoluteFill}
+            />
+          ),
+          unmountOnBlur: true
+        }}
+      >
+        <Tab.Screen 
+          name="Home" 
+          component={Home} 
+          options={HomeOptions}
+        />
       </Tab.Navigator>
+
+      <BlurView 
+        tint={scheme}
+        intensity={100}
+        style={{
+          width: "100%",
+		      height: Constants.statusBarHeight
+        }}
+      />
+
+      <StatusBar 
+        style={"auto"}
+      />
     </NavigationContainer>
-		
 	);
 }
--- screens/home/Home.js
+++ screens/home/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 import HomeScene from "./HomeScene";
+import { MaterialIcons } from "@expo/vector-icons";
 import { ViroARSceneNavigator } from '@viro-community/react-viro';
 
 export const HomeOptions = {
-    title: "Home"
+    title: "Home",
+	headerShown: false,
+	tabBarLabel: "Home",
+	tabBarIcon: ({ color, size }) => (
+		<MaterialIcons name="home" size={size} color={color} />
+	),
 }
 
 export const Home = () => {
